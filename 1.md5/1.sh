26e5ec722310fa98ed77e2bd788c7694f7a5fc3c
ultramaxim <ultramaximka@gmail.com>
2011-09-26 19:17:09 +0600

    проверка на параметры

--- 1.md5/1.sh
+++ 1.md5/1.sh
@@ -1,8 +1,12 @@
-declare -A array
-while read b c; do
-	while read a && [[ -n $a ]]; do
-		array[$a]="$b $c";
-	done 
-done <$1
-
+if test -z "$1"
+then 
+	echo "Usage: 1.sh filenameFirst filenameSecond"
+else
+	declare -A array
+	while read b c; do
+		while read a && [[ -n $a ]]; do
+			array[$a]="$b $c";
+		done 
+	done <$1
+fi
 

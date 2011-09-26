1ebde23d150eb87be3de5d6cb54eceab79580215
ultramaxim <ultramaximka@gmail.com>
2011-09-26 19:35:46 +0600

    next

--- 1.md5/1.sh
+++ 1.md5/1.sh
@@ -5,8 +5,15 @@ else
 	declare -A array
 	while read b c; do
 		while read a && [[ -n $a ]]; do
-			array[$a]="$b $c";
+			array[$a]="$b::$c"
 		done 
 	done <$1
-fi
+	
+	while read a b c; do
+		if [[ ${array[$c]} != "$a::$b" ]]
+		then
+			echo "[Name::Type::Value]:$c::${array[$c]} -----> $c::$a::$b"
+		fi;
+	done <$2
+fi;
 

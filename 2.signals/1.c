a13f964da1499e74d850e514b6c80757217b1ade
ultramaxim <ultramaximka@gmail.com>
2011-12-02 21:16:10 +0600

    переделываю

--- 2.signals/1.c
+++ 2.signals/1.c
@@ -7,22 +7,14 @@ char info[100];
 
 void term_handler(int i,siginfo_t *c,void *v) 
 {
-  write(1,info,strlen(info));
-  
+  	write(1,info,strlen(info));
+  	if(i==2)
+    		exit(0);
 }
 
-int main(int argc, char ** argv) 
+int main() 
 {
-  sigset_t sset;
-  int sig;
-  struct sigaction sa;
-  sa.sa_handler = term_handler;
-  sigaction(SIGTERM, &sa, 0);
-  sigfillset(&sset);
-  sigdelset(&sset, SIGTERM);
-  sigprocmask(SIG_SETMASK, &sset, 0);
-  printf("My pid is %i\n", getpid());
-  while(!sigwait(&sset, &sig))
-    printf("Signal %i - %s\n", sig, sys_siglist[sig]);
-  return EXIT_SUCCESS;
+  	sprinf(info,"PID: %d; GID %d\n",getpid(),getpgid(getpid()));
+	write(1,info,strlen(info));
+	
 }

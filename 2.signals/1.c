7d2831d0a6c61ea7936ad9663d58b7e6520afe95
ultramaxim <ultramaximka@gmail.com>
2011-12-02 21:32:42 +0600

    аштфд

--- 2.signals/1.c
+++ 2.signals/1.c
@@ -14,10 +14,23 @@ void term_handler(int i,siginfo_t *c,void *v)
 
 int main() 
 {
-  	sprinf(info,"PID: %d; GID %d\n",getpid(),getpgid(getpid()));
+  	sprintf(info,"PID: %d; GID %d\n",getpid(),getpgid(getpid()));
 	write(1,info,strlen(info));
 	struct sigaction sa;
 	sa.sa_sigaction=term_handler;
 	sa.sa_flags=SA_SIGINFO;
 	sigfillset(&sa.sa_mask);	
+	int i;
+	for(i=1;i<32;i++)
+	{
+		if(i!=9 && i!=19)
+		{
+			if(sigaction(i,&sa,0)==-1)
+				printf("Error: %d\n",i);
+		}
+	}
+
+	while(1) sleep(1);
+
+	return 0;
 }

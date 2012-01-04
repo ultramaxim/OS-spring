6eca9e4afeed0dc9715885d83a38f048535de325
ultramaxim <ultramaximka@gmail.com>
2012-01-04 16:53:20 +0600

    исправления

--- 2.signals/1.c
+++ 2.signals/1.c
@@ -5,7 +5,7 @@
 
 char info[100];
 
-void term_handler(int i,siginfo_t *c,void *v) 
+void func(int i,siginfo_t *c,void *v) 
 {
   	write(1,info,strlen(info));
   	if(i==2)
@@ -16,20 +16,21 @@ int main()
 {
   	sprintf(info,"PID: %d; GID %d\n",getpid(),getpgid(getpid()));
 	write(1,info,strlen(info));
+	
 	struct sigaction sa;
-	sa.sa_sigaction=term_handler;
+	sa.sa_sigaction=func;
 	sa.sa_flags=SA_SIGINFO;
-	sigfillset(&sa.sa_mask);	
+	if(sigfillset(&sa.sa_mask)==-1)
+	{
+		puts("Can't set sa_mask");
+		return 1;
+	}	
 	
 	int i;
 	for(i=1;i<32;i++)
-	{
 		if(i!=9 && i!=19)
-		{
 			if(sigaction(i,&sa,0)==-1)
 				printf("Error: %d\n",i);
-		}
-	}
 
 	while(1) sleep(1);
 

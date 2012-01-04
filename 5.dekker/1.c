3cf3e0fcf25762f29021a42448f8026099032d23
ultramaxim <ultramaximka@gmail.com>
2012-01-04 19:56:13 +0600

    dekker

---
+++ 5.dekker/1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <pthread.h>
+
+volatile char flag[2] = { 0, 0 };
+volatile char turn    = 0;
+volatile int  val     = 0;
+
+static void* func( void* id ) 
+{
+	int self  = (int)id;
+    	int other = 1 - self;
+	volatile int countdown = 1000000;
+    	
+	while( countdown >= 0 ) 
+   	{
+        	flag[self] = 1;
+       	 	while( flag[other] == 1 ) 
+		{
+            		if( turn != self ) 
+			{
+                		flag[self] = 0;
+                		while( turn != self );
+                		flag[self] = 1;
+            		}
+        	}
+        	// critical section
+        	if( --countdown >= 0 ) val++;
+        	// end
+        	turn = other;
+	        flag[self] = 0;
+    	}
+}
+
+int main() {
+    
+	pthread_t thread[2];
+
+	pthread_create(&thread[0], NULL, &func, (void*)0);
+	pthread_create(&thread[1], NULL, &func, (void*)1);
+    
+	pthread_join(thread[0], NULL);
+	pthread_join(thread[1], NULL);
+	
+	printf("%d\n", val);
+	
+    return 0;
+}
+

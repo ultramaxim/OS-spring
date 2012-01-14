fa7e2c9d9877f1ddc632b914569b0666938b262a
ultramaxim <ultramaximka@gmail.com>
2012-01-14 16:15:13 +0600

    исправления

--- 2.signals/1.c
+++ 2.signals/1.c
@@ -1,37 +1,37 @@
 #include <stdio.h>
+#include<unistd.h>//for pipe
 #include <stdlib.h>
 #include <signal.h>
 #include <string.h>
 
-char info[100];
+int channel[2];
 
 void func(int i,siginfo_t *c,void *v) 
 {
-  	write(1,info,strlen(info));
-  	if(i==2)
-    		exit(0);
+	
 }
 
 int main() 
 {
-  	sprintf(info,"PID: %d; GID %d\n",getpid(),getpgid(getpid()));
-	write(1,info,strlen(info));
-	
+  	if(pipe(channel)==-1)
+	{
+		puts("Can't create pipe");
+		return 1;
+	}
 	struct sigaction sa;
 	sa.sa_sigaction=func;
 	sa.sa_flags=SA_SIGINFO;
 	if(sigfillset(&sa.sa_mask)==-1)
 	{
 		puts("Can't set sa_mask");
-		return 1;
-	}	
-	
+		return 2;
+	}
 	int i;
 	for(i=1;i<32;i++)
 		if(i!=9 && i!=19 && sigaction(i,&sa,NULL))
 		{
 			printf("Can't set signal %d\n",i);
-			return 2;
+			return 3\;
 		}
 
 	while(1) sleep(1);
